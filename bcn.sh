1210a2623d4c9d8ff9d5cc5666cfcf6dc23ae9b8
7i6i5 <35090421+7i6i5@users.noreply.github.com>
2018-01-03 23:01:12 -0600

    Add files via upload

---
+++ bcn.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo Script para bcn
+minerd -a cryptonight -o stratum+tcp://bcn.pool.minergate.com:45550 -u endeath9999@gmail.com -p x 
---
+++ xmr.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo Script para xmr
+minerd -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45560 -u endeath9999@gmail.com -p x
